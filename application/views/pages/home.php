6800bbfa32a83f8733b2715f6800bcdce8aa044b
Jake Zerrer <him@jakezerrer.com>
2012-08-11 16:25:47 -0400

    More work on main view

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,17 +1,24 @@
-
 	<section id="box_list">
-		<a href="new_box">New Box</a>
-		<ul>
-			<li><a href="select_box_1">Box 1</li>
-			<li><a href="select_box_2">Box 2</li>
-			<li><a href="select_box_3">Box 3</li>
+		<ul id="box_commands">
+			<li><a href="#new_box">New Box</a></li>
+			<li><a href="#delete_box">Delete Box</a></li>
+		</ul>
+		<table>
+			<caption>Box List</caption>
+			<tr id="box_1"><td><a href="#select_box_1">Box 1</a></td></tr>
+			<tr id="box_2"><td><a href="#select_box_2">Box 2</a></td></tr>
+			<tr id="box_3"><td><a href="#select_box_3">Box 3</a></td></tr>
+		</table>
 	</section>
 	<section id="item_list">
-		<a href="#new_item">New Item</a>
+		<ul li="item_commands">
+			<li><a href="#new_item">New Item</a></li>
+			<li><a href="#delete_item">Delete Item</a></li>
 		<table>
-			<tr><td>Item 1 Number</td><td>Item 1 Description</td></tr>
-			<tr><td>Item 2 Number</td><td>Item 2 Description</td></tr>
-			<tr><td>Item 3 Number</td><td>Item 3 Description</td></tr>
+			<caption>Item List</caption>
+			<tr id="item_1"><td>Item 1 Number</td><td>Item 1 Description</td></tr>
+			<tr id="item_2"><td>Item 2 Number</td><td>Item 2 Description</td></tr>
+			<tr id="item_3"><td>Item 3 Number</td><td>Item 3 Description</td></tr>
 			</tr>
 		</table>
 	</section>
