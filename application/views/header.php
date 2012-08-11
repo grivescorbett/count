3e55aa8845bf9379a8a39ee3dcd89af6c5802cb2
Jake Zerrer <telekid@Jakes-MacBook-Pro.local>
2012-08-11 16:00:15 -0400

    Added initial view structure

---
+++ application/views/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<title>Count</title>
+	<link rel="stylesheet" href="css/master.css" type="text/css" media="screen" charset="utf-8">
+</head>
+<body>
---
+++ application/views/home.php
@@ -0,0 +1,17 @@
+
+	<section id="box_list">
+		<a href="new_box">New Box</a>
+		<ul>
+			<li><a href="select_box_1">Box 1</li>
+			<li><a href="select_box_2">Box 2</li>
+			<li><a href="select_box_3">Box 3</li>
+	</section>
+	<section id="item_list">
+		<a href="#new_item">New Item</a>
+		<table>
+			<tr><td>Item 1 Number</td><td>Item 1 Description</td></tr>
+			<tr><td>Item 2 Number</td><td>Item 2 Description</td></tr>
+			<tr><td>Item 3 Number</td><td>Item 3 Description</td></tr>
+			</tr>
+		</table>
+	</section>
