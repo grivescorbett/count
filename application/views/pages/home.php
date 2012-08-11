7c790608c8ce1c742d67289302f3839b7272abbc
Jake Zerrer <him@jakezerrer.com>
2012-08-11 16:34:48 -0400

    Fixed headers

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,22 +1,22 @@
 <script type="text/javascript" src="scripts/home.js"></script>  
 	<section id="box_list">
+		<h2>Box List</h2>
 		<ul id="box_commands">
 			<li><a id="new_box" href="">New Box</a></li>
 			<li><a id="delete_box" href="">Delete Box</a></li>
 		</ul>
 		<table>
-			<caption>Box List</caption>
 			<tr id="box_1"><td><a href="#select_box_1">Box 1</a></td></tr>
 			<tr id="box_2"><td><a href="#select_box_2">Box 2</a></td></tr>
 			<tr id="box_3"><td><a href="#select_box_3">Box 3</a></td></tr>
 		</table>
 	</section>
 	<section id="item_list">
+		<h2>Box 1 Contents</h2>
 		<ul li="item_commands">
 			<li><a id="new_item" href="">New Item</a></li>
 			<li><a id="delete_item" href="">Delete Item</a></li>
 		<table>
-			<caption>Item List</caption>
 			<tr id="item_1"><td>Item 1 Number</td><td>Item 1 Description</td></tr>
 			<tr id="item_2"><td>Item 2 Number</td><td>Item 2 Description</td></tr>
 			<tr id="item_3"><td>Item 3 Number</td><td>Item 3 Description</td></tr>
